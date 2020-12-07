c7eb639f1eda6afe050c2d6359ca1f6b26fcd144
kirbyzhou <kirbyzhou@protonmail.com>
2020-12-06 20:50:35 -0800

    Clean up

--- src/graph-gl-core.js
+++ src/graph-gl-core.js
@@ -21,7 +21,6 @@ export default class GraphGL {
     for (const property in settings) {
       this.settings[property] = settings[property]
     }
-    // console.log(this.settings)
     runtimeSettings.resetSettings(this.settings)
     // Initialize renderer
     this.renderer = new GLRenderer(this.settings)
@@ -35,14 +34,10 @@ export default class GraphGL {
     this.control.update()
     this.control.control.target.set(0, 0, 0)
     this.control.control.addEventListener('change', this.render.bind(this))
-    // if (this.settings.debug) {
-    //   this.addDebugCameraControl()
-    // }
     this.refresh()
   }
 
   nodes () {
-    // console.log('nodes core', this.scene.graphNodes())
     return this.scene.graphNodes()
   }
 
@@ -72,7 +67,6 @@ export default class GraphGL {
   }
 
   refresh () {
-    // Update container dimensions
     const dim = getDimensions(this.settings.selector)
     runtimeSettings.setOneSetting({ key: 'width', value: dim.width })
     runtimeSettings.setOneSetting({ key: 'height', value: dim.height })
@@ -87,29 +81,17 @@ export default class GraphGL {
     const centerY = (this.scene.boundingBox.yMax + this.scene.boundingBox.yMin) / 2
     const bondX = Math.abs(this.scene.boundingBox.xMax - this.scene.boundingBox.xMin) + runtimeSettings.settings.viewportPadding * 2
     const bondY = Math.abs(this.scene.boundingBox.yMax - this.scene.boundingBox.yMin) + runtimeSettings.settings.viewportPadding * 2
-    // console.log(this.camera.camera.zoom)
     const bond = bondX > bondY ? bondX : bondY
     if (runtimeSettings.settings.width / runtimeSettings.settings.height > 1.0) {
-      // console.log(runtimeSettings.settings.height / (bond))
       this.camera.camera.zoom = runtimeSettings.settings.height / (bond)
     } else {
-      // console.log(runtimeSettings.settings.width / (bond))
       this.camera.camera.zoom = runtimeSettings.settings.width / (bond)
     }
-    // console.log(this.camera.camera.zoom)
     this.camera.camera.position.set(centerX, centerY, this.camera.camera.position.z)
     this.camera.camera.updateProjectionMatrix()
   }
 
-  // addDebugCameraControl () {
-  //   this.controls = new CameraControls(this.camera.camera, this.renderer.domElement)
-  //   this.controls.addEventListener('change', () => {
-  //     this.controls.update()
-  //     this.renderer.render(this.scene.scene, this.camera.camera)
-  //   })
-  // }
   render () {
-    // this.control.update()
     this.scene.sceneUpdate()
     this.renderer.render(this.scene.scene, this.camera.camera)
   }
